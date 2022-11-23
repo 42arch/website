20a0790525b9e77163f74aec67ea50d2b3f74bbf
RE0x2A <rend42@163.com>
2022-11-24 00:22:19 +0800

    post styles

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -19,18 +19,30 @@ const index: NextPage<{ allPostsData: PostData[] }> = ({ allPostsData }) => {
       <div className="m-auto py-4 mt-10">
         {allPostsData.map((item, idx) => {
           return (
-            <section key={idx} className="pt-6">
+            <section key={idx} className="pt-6 pb-2">
               <h1 className="font-bold text-xl py-1 w-fit text-th-text-h hover:opacity-80 transition-all">
                 <Link href={`/post/${item.slug}`} className="w-fit">
                   {item.title}
                 </Link>
               </h1>
-              <div className="text-xs py-1">
+              <div className="text-xs py-1 flex">
                 <time dateTime="">{dayjs(item.date).format('YYYY-MM-DD')}</time>
               </div>
               <span className="inline-block my-2 py-1 text-sm">
                 {item.excerpt}
               </span>
+
+              <div className="text-xs py-1">
+                {item.tags.map((tag, i) => {
+                  return (
+                    <span
+                      key={i}
+                      className="mr-4 hover:opacity-80 cursor-pointer">
+                      {tag}
+                    </span>
+                  )
+                })}
+              </div>
             </section>
           )
         })}
