381cf70465e1e5491a7cffa1be31c67e5cca4bd6
42arch <rend42@163.com>
2023-01-27 22:58:24 +0800

    chore: init v3

--- components/DateTime.tsx
+++
@@ -1,9 +0,0 @@
-import dayjs from 'dayjs'
-import { FC } from 'react'
-
-const DateTime: FC<{ datetime: number }> = ({ datetime }) => {
-  const formatdt = dayjs(datetime).format('YYYY-MM-DD')
-  return <time dateTime={formatdt}>{formatdt}</time>
-}
-
-export default DateTime
