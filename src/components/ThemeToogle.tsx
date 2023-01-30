8c398ebec99f19c31b0b2f604ca96b12f6fe6a34
42arch <rend42@163.com>
2023-01-31 00:49:53 +0800

    fix: theme-toggle

--- src/components/ThemeToogle.tsx
+++ src/components/ThemeToogle.tsx
@@ -7,14 +7,26 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger
 } from './ui/DropdownMenu'
+import { useMounted } from '@/hooks/useMounted'
+
+const className =
+  'cursor-pointer hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100'
 
 const ThemeToogle = () => {
   const { theme, setTheme } = useTheme()
+  const mounted = useMounted()
+
+  const themeMap: Record<string, JSX.Element> = {
+    light: <FiSun className={className} />,
+    dark: <FiMoon className={className} />,
+    system: <FiMonitor className={className} />
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div>
-          <FiSun className="cursor-pointer hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-100" />
+          {mounted && theme && themeMap[theme]}
           <span className="sr-only">Toggle theme</span>
         </div>
       </DropdownMenuTrigger>
