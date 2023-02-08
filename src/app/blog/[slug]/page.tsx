a70626de591a29e71dae601a931df6a6f50e57b5
42arch <rend42@163.com>
2023-02-09 00:22:44 +0800

    update post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,7 @@
+import PostTag from '@/components/PostTag'
 import { allPosts } from 'contentlayer/generated'
 import { format, parseISO } from 'date-fns'
-// import Link from 'next/link'
-// import { FiChevronLeft } from 'react-icons/fi'
+import Link from 'next/link'
 
 interface IProps {
   params: {
@@ -28,25 +28,98 @@ export default async function Page({ params }: IProps) {
   return (
     <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <article>
-        {/* <Link
-          href="/blog"
-          className="absolute top-14 -left-[200px] hidden items-center justify-center text-sm font-medium text-slate-600 hover:text-slate-900 xl:inline-flex">
-          <FiChevronLeft className="mr-2 h-4 w-4" />
-          See all posts
-        </Link> */}
-
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-          <div className="prose dark:prose-invert">
-            <div className="mb-6 text-center">
-              <h1 className="mb-1 text-3xl font-bold">{post.title}</h1>
-              <time dateTime={post.date} className="text-sm text-slate-600">
-                {format(parseISO(post.date), 'LLLL d, yyyy')}
-              </time>
+          <header className="pt-6 xl:pb-6">
+            <div className="space-y-1 text-center">
+              <dl className="space-y-10">
+                <div>
+                  <dt className="sr-only">Published on</dt>
+                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                    <time
+                      dateTime={post.date}
+                      className="text-sm text-slate-600">
+                      {format(parseISO(post.date), 'LLLL d, yyyy')}
+                    </time>
+                  </dd>
+                </div>
+              </dl>
+              <div>
+                <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+                  {post.title}
+                </h1>
+              </div>
             </div>
-            <div
-              className="cl-post-body"
-              dangerouslySetInnerHTML={{ __html: post.body.html }}
-            />
+          </header>
+          <div
+            className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0"
+            style={{ gridTemplateRows: 'auto 1fr' }}>
+            <dl className="pt-6 pb-10 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700"></dl>
+            <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
+              <div className="prose dark:prose-invert max-w-none pt-10 pb-8">
+                <div
+                  className="cl-post-body"
+                  dangerouslySetInnerHTML={{ __html: post.body.html }}
+                />
+              </div>
+              <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
+                {/* <Link href={discussUrl(slug)} rel="nofollow">
+                  {'Discuss on Twitter'}
+                </Link>
+                {` • `}
+                <Link href={editUrl(fileName)}>{'View on GitHub'}</Link> */}
+              </div>
+            </div>
+            <footer>
+              <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
+                {post.tags && (
+                  <div className="py-4 xl:py-8">
+                    <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                      Tags
+                    </h2>
+                    <div className="flex flex-wrap">
+                      {post.tags.map((tag) => (
+                        <PostTag key={tag} text={tag} />
+                      ))}
+                    </div>
+                  </div>
+                )}
+                {/* {(next || prev) && (
+                    <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
+                      {prev && (
+                        <div>
+                          <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                            Previous Article
+                          </h2>
+                          <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                            <Link href={`/blog/${prev.slug}`}>
+                              {prev.title}
+                            </Link>
+                          </div>
+                        </div>
+                      )}
+                      {next && (
+                        <div>
+                          <h2 className="text-xs uppercase tracking-wide text-gray-500 dark:text-gray-400">
+                            Next Article
+                          </h2>
+                          <div className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                            <Link href={`/blog/${next.slug}`}>
+                              {next.title}
+                            </Link>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  )} */}
+              </div>
+              <div className="pt-4 xl:pt-8">
+                <Link
+                  href="/blog"
+                  className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+                  &larr; Back to the blog
+                </Link>
+              </div>
+            </footer>
           </div>
         </div>
       </article>
---
+++ src/components/PostTag.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React, { FC } from 'react'
+
+interface PostTagProps {
+  text: string
+}
+
+const PostTag: FC<PostTagProps> = ({ text }) => {
+  return (
+    <Link
+      href={'/'}
+      className=' className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"'>
+      {text.split(' ').join('-')}
+    </Link>
+  )
+}
+
+export default PostTag
