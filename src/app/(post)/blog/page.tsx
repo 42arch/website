08e997c311e8698c0b0cf6bae3e31f64fa8bb85f
42arch <rend42@163.com>
2023-04-27 21:31:56 +0800

    fix: :lipstick: styles

--- src/app/(post)/blog/page.tsx
+++ src/app/(post)/blog/page.tsx
@@ -9,13 +9,13 @@ const BlogPage: FC = () => {
   })
 
   return (
-    <div className="container max-w-4xl py-6 lg:py-10">
+    <div className="container py-6 lg:py-10">
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-4xl font-extrabold leading-9 tracking-tight text-slate-900 dark:text-slate-100 md:text-6xl md:leading-14">
           All Posts
         </h1>
       </div>
-      <div className="grid grid-cols-2 gap-x-6 gap-y-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-10">
         {posts.map((post, idx) => (
           <PostCard key={idx} {...post} />
         ))}
