980e135aad5e9de0dd130c5c085b655e28f41b5b
42arch <rend42@163.com>
2023-05-07 23:33:17 +0800

    new index slogan

--- src/app/(common)/page.tsx
+++ src/app/(common)/page.tsx
@@ -12,11 +12,12 @@ export default function Home() {
           priority
         />
         <h1 className="text-3xl font-bold leading-[1.1] tracking-tighter sm:text-5xl md:text-6xl">
-          What's going on here?
+          Be curious. Read widely. Try new things.
         </h1>
-        <p className="max-w-[42rem] leading-normal text-slate-600 dark:text-slate-400 sm:text-xl sm:leading-8">
-          I'm building a web app with Next.js 13 and open sourcing everything.
-          Follow along as we figure this out together.
+        <p className="leading-normal text-slate-600 dark:text-slate-400 sm:text-xl sm:leading-8 text-right">
+          {/* I'm building a web app with Next.js 13 and open sourcing everything.
+          Follow along as we figure this out together. */}
+          -- Aaron Swartz
         </p>
       </section>
     </div>
